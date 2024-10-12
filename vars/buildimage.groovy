fc3229870215232cd4ea662dc06c9ce43c1b6087
m-bilal-khan <bilalkhan.192003@gmail.com>
2024-10-12 17:59:52 +0500

    minor changes

--- vars/buildimage.groovy
+++ vars/buildimage.groovy
@@ -2,9 +2,9 @@
 
 def call(){
   echo "building the docker image..."
-  withCredentials([usernamePassword(credentialsId: 'docker-hub-repo-creds', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-    sh 'docker build -t mbilalkhan/practice-repo:1.0.0 .'
-    sh 'echo $PASS | docker login -u $USER --password-stdin'
-    sh 'docker push mbilalkhan/practice-repo:1.0.0'
-  }
+  // withCredentials([usernamePassword(credentialsId: 'docker-hub-repo-creds', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+  //   sh 'docker build -t mbilalkhan/practice-repo:1.0.0 .'
+  //   sh 'echo $PASS | docker login -u $USER --password-stdin'
+  //   sh 'docker push mbilalkhan/practice-repo:1.0.0'
+  // }
 }
