2146542291e574c281cc93c45e56754badff585d
m-bilal-khan <bilalkhan.192003@gmail.com>
2024-10-17 23:58:41 +0500

    Bug Fixes

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,6 +1,6 @@
 #!/user/bin/env groovy
 
-import.com.example.Docker
+import com.example.Docker
 def call(String imageName){
   return new Docker(this).buildDockerImage(imageName) //by this we passed the reference to all variable availabe to jenkins file to our docker class
 }
